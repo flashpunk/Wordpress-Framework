8be7a01f10e8580ff3839d58358686a246ff7299
Michael Caputo <mike@flashpunk.ca>
2013-03-11 10:45:57 -0400

    Removing unnecessary simplecart function

--- functions.php
+++ functions.php
@@ -6,17 +6,3 @@ require_once(TEMPLATEPATH . '/framework/init.php');
 
 //Thumbnail Loader
 //require_once(TEMPLATEPATH . '/framework/theme-specific/thumbnails.php');
-
-
-
-function simple_cart_total_itmes() {
-    $total = 0;
-    if (is_array($_SESSION['simpleCart'])) {
-
-	    foreach ( $_SESSION['simpleCart'] as $item ) {
-	        $total ++;
-	    }
-    	return $total;
-	}
-}
-
